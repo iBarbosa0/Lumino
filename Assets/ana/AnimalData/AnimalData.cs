4ff1b6ce43a67d8559d4b40452ceda4f466cb847
Votexdcx <Votexdcx@gmail.com>
2025-07-06 13:43:01 +0100

    some fixes

---
+++ Assets/ana/AnimalData/AnimalData.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "AnimalData", menuName = "Animal/Create New Animal")]
+public class AnimalData : ScriptableObject
+{
+    public string animalName;
+
+    public AnimationClip idleAnim;
+    public AnimationClip eatAnim;
+    public AnimationClip playAnim;
+
+    public GameObject foodPrefab;
+    public GameObject toyPrefab;
+
+    public AudioClip idleAudio;
+    public AudioClip eatAudio;
+    public AudioClip playAudio;
+}
