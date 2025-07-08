326a414a73c35e2f7c03331fa7cfdf4762379ff1
Votexdcx <Votexdcx@gmail.com>
2025-07-08 21:35:05 +0100

    merge fixes

---
+++ Assets/ana/animalsbuttonscript.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class animalsbuttonscript : MonoBehaviour
+{
+   public void Play()
+    {
+        SceneManager.LoadScene("Scene Pickanimals");
+    }
+}
